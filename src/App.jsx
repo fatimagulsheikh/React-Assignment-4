5557fd0bc52d763dea619094fb1c4e7514d749be
Fatima Raees <gull33416@gmail.com>
2026-04-22 17:56:14 +0500

    React-Assignment-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,64 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import './App.css'
+import { GlobalContext } from './context/Context' // ⚠️ capital C
+
+import { IoMdSunny } from 'react-icons/io'
+import { IoMoonSharp } from 'react-icons/io5'
 
 const App = () => {
+  const { state, dispatch } = useContext(GlobalContext);
+  
+
+  const handleClick = () => {
+    dispatch({ type: "CHANGE_THEME" });
+  };
+
   return (
-    <BrowserRouter>
+    // ✅ yahan main change hai (TOP LEVEL WRAPPER)
+    <div className={state?.darkTheme ? "dark" : "light"}>
 
-      {/* Header */}
-      <header className="header">
-        <div className="logo">MyWebsite</div>
+      <BrowserRouter>
 
-        <nav className="nav">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
-      </header>
+        {/* Header */}
+        <header className="header">
+          <div className="logo">MyWebsite</div>
 
-      {/* Pages */}
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+          <nav className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
 
-    </BrowserRouter>
+            {state?.darkTheme ? (
+              <IoMdSunny className='icon' onClick={handleClick} />
+            ) : (
+              <IoMoonSharp className='icon' onClick={handleClick} />
+            )}
+          </nav>
+        </header>
+
+        {/* Pages */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
+
